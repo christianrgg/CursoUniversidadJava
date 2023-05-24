7793769dd0bca1cf3363a32dce6a24f94914dc70
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-24 12:08:20 -0600

    Hola Mundo

---
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,18 @@
+
+public class HolaMundo {
+    public static void main(String args[]){
+        System.out.println("Hola Mundo desde Java");
+    }
+}
+
+
+/*PASOS PARA CREAR PROYECTO SENCILLO EN JAVA 
+	1. Clic en new Proyect
+	2. Seleccionar java with ant
+	3. En projects seleccionar Java with aplication
+	4. Clic en next y esperar a que se habilite la caracteristica del ide
+	5. Colocar nombre del proyecto "HolaMundoJava"
+	6. Colocar ubicación del proyecto lo más cercana a la raiz
+	7. Crear clase HolaMundo
+	8. Clic en finish
+*/
