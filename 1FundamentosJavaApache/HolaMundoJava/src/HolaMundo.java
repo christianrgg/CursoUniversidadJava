1a28f0c0db5a2ac1f7f7253b9e798b8a9e38d102
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-25 10:28:08 -0600

    Tipos primitivos: Ejercicio Detalles de Libro

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -1,34 +1,53 @@
 
 import java.util.Scanner;
-
+//Se solicita capturar la siguiente información de una tienda de libros:
+//
+//nombre (String)
+//
+//id (int)
+//
+//precio (double)
+//
+//envioGratuito (boolean)
+//
+//Tu tarea es imprimir un mensaje en el siguiente formato:
+//
+//Proporciona el nombre:
+//Proporciona el id:
+//Proporciona el precio:
+//Proporciona el envio gratuito:
+//<nombre> #<id>
+//Precio: <simbolo><precio>
+//Envio Gratuito: <envioGratuito>
+//Por ejemplo:
+//
+//Proporciona el nombre:
+//Programacion con Java
+//Proporciona el id:
+//1520
+//Proporciona el precio:
+//899
+//Proporciona el envio gratuito:
+//true
+//Programacion con Java #1520
+//Precio: $899.00
+//Envio Gratuito: true
 
 public class HolaMundo {
 
     public static void main(String args[]) {
-      //Convertir tipo String a tipo int
-      var edad = Integer.parseInt("20");
-        System.out.println("edad = " + (edad + 1));
-        
-        var valorPi = Double.parseDouble("3.1416");
-        System.out.println("valorPi = " + valorPi);
-        
-        //Pedir un valor
-        /*var consola = new Scanner(System.in);
-        System.out.println("Cual es tu edad?");
-        edad = Integer.parseInt(consola.nextLine());
-        System.out.println("edad = " + edad);*/
-        
-        var edadTexto = String.valueOf(10);
-        System.out.println("edadTexto = " + edadTexto);
-        
-        //Extraer un caracter de una cadena y asignarlo a una variable char
-        var caracter = "hola".charAt(0);
-        System.out.println("caracter = " + caracter);
-        var consola = new Scanner(System.in);
-        System.out.println("Proporciona un caracter");
-        caracter = consola.nextLine().charAt(0);
-        System.out.println("caracter = " + caracter);
-        
+        System.out.println("Proporciona el nombre del libro:");
+        Scanner consola = new Scanner(System.in);
+        var nombre = consola.nextLine();
+        System.out.println("Proporciona el ID:");
+        int id = Integer.parseInt(consola.nextLine());
+        System.out.println("Proporciona el precio:");
+        double precio = Double.parseDouble(consola.nextLine());
+        System.out.println("Proporciona el envio gratuito:");
+        boolean envioGratuito = Boolean.parseBoolean(consola.nextLine());
+        System.out.println(nombre + " " + "#ID" + " " + id);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Envio gratuito: " + envioGratuito);
 
     }
 }
