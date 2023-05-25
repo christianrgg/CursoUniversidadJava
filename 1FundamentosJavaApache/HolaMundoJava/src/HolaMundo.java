fc5ddf21be595b848b20c81f3d11bd96a2cd6ccd
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-24 19:35:14 -0600

    Tipos primitivos: Introduccion

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        System.out.println("Escribe el titulo del libro");
-        Scanner consola = new Scanner(System.in);
-        var titulo = consola.nextLine();
-        System.out.println("Escribe el nombre del autor");
-        var autor = consola.nextLine();
-        System.out.println("El libro" +" "+ titulo + " " + "fue escrito por" + " " + autor);
+       /*
+        Tipos primitivos enteros: byte(8 bits), short(1 bits), int(32bits), long (64 bits)
+        */
+       
+       byte numeroByte = 10;
+        System.out.println("Valor minimo byte" + Byte.MIN_VALUE);
+        System.out.println("Valor maximo byte" + Byte.MAX_VALUE);
+        //A partir del numero 128 ya no se guardará o copilará marcara perdida de presicion
     }
 }
