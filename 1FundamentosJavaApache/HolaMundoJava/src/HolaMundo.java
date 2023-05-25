438e15c1cd7fdbbbde4685ea47668df7aa34d62e
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-25 16:59:40 -0600

    Operadores en JAVA: Aritmeticos

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -1,53 +1,44 @@
 
-import java.util.Scanner;
-//Se solicita capturar la siguiente información de una tienda de libros:
-//
-//nombre (String)
-//
-//id (int)
-//
-//precio (double)
-//
-//envioGratuito (boolean)
-//
-//Tu tarea es imprimir un mensaje en el siguiente formato:
-//
-//Proporciona el nombre:
-//Proporciona el id:
-//Proporciona el precio:
-//Proporciona el envio gratuito:
-//<nombre> #<id>
-//Precio: <simbolo><precio>
-//Envio Gratuito: <envioGratuito>
-//Por ejemplo:
-//
-//Proporciona el nombre:
-//Programacion con Java
-//Proporciona el id:
-//1520
-//Proporciona el precio:
-//899
-//Proporciona el envio gratuito:
-//true
-//Programacion con Java #1520
-//Precio: $899.00
-//Envio Gratuito: true
+/*
+Operadores aritmeticos: +, -, *, /, %
+Operadores de relacion: <, >, <=, >=, !=, ==
+Operadores logicos: && o &, || o |, !, ^
+Operadores unitarios: ~, -
+Operadores a nivel de bits: &, |, ^, <<, >>, >>>
+Operadores de asignación: ++, --, =, *=, /=, %=, +=, -=, <<=, >>=, >>>=
+Operador condicional: ?:
+Prioridad y orden de evaluación: (), [], ., - ~ ! ++ --, new(tipo)expresion, << >> >>>, < > <= >=, == !=, &, ||, ?:
+
+
+*/
 
 public class HolaMundo {
 
     public static void main(String args[]) {
-        System.out.println("Proporciona el nombre del libro:");
-        Scanner consola = new Scanner(System.in);
-        var nombre = consola.nextLine();
-        System.out.println("Proporciona el ID:");
-        int id = Integer.parseInt(consola.nextLine());
-        System.out.println("Proporciona el precio:");
-        double precio = Double.parseDouble(consola.nextLine());
-        System.out.println("Proporciona el envio gratuito:");
-        boolean envioGratuito = Boolean.parseBoolean(consola.nextLine());
-        System.out.println(nombre + " " + "#ID" + " " + id);
-        System.out.println("Precio: $" + precio);
-        System.out.println("Envio gratuito: " + envioGratuito);
+        int a=3, b=2;
+        var resultado = a +b;
+        System.out.println("resultado de la suma= " + resultado);
+        
+        resultado = a - b;
+        System.out.println("resultado de la resta= " + resultado);
+        
+        resultado = a * b;
+        System.out.println("resultado de multiplicación = " + resultado);
+        
+        resultado = a / b;
+        System.out.println("resultado de  división= " + resultado);
+        
+        var resultado2 = 3.2/b;
+        System.out.println("resultado2 = " + resultado2);
+        
+        resultado = a % b;
+        System.out.println("resultado modulo = " + resultado);
+        
+        //Si solo es una linea de codigo lo que ira dentro del if o else se pueden omitir las llaves
+        if(b % 2 == 0 ) 
+            System.out.println("Es numero par");
+        else 
+            System.out.println("Es numero impar");
 
     }
 }
