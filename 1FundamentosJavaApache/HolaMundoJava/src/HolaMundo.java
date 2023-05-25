43a4400f07923d7a080fe16078c2755749f7cd48
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-24 18:31:37 -0600

    Fundamentos: Clase Scanner

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -1,23 +1,17 @@
 
+import java.util.Scanner;
+
+
 public class HolaMundo {
 
     public static void main(String args[]) {
-        // Con diagonal invertida + n "\n" da un salto de linea
-        var nombre = "Saira";
-        System.out.println("Nueva linea: \n" + nombre);
-        
-        // Con diagonal invertida + t "\t" da un sangria de espacio
-        System.out.println("Sangria: \t" + nombre);    
-        
-        // Caracter de retroceso Elimina el ultimo caracter (De acuerdo a las veces que lo escribas)
-        System.out.println("Sangria: \b" + nombre); 
-        System.out.println("Sangria: \b\b" + nombre); 
-        
-        // Caracter de comillas simples coloca todo entre comillas
-        System.out.println("Comilla: \'" + nombre + "\'"); 
-        
-        // Caracter de comillas dobles coloca todo entre comillas dobles \"
-        System.out.println("Comilla: \"" + nombre + "\""); 
+        System.out.println("Escribe tu nombre:");
+        Scanner consola = new Scanner(System.in);
+        var usuario = consola.nextLine();
+        System.out.println("usuario = " + usuario);
+        System.out.println("Escribe tu grado");
+        var titulo = consola.nextLine();
+        System.out.println("Resultado:" + titulo + " " + usuario);
         
     }
 }
