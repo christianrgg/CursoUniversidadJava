ee08d4357d1c2f33e4c96813d7a63bbb378f8f4f
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-24 23:43:51 -0600

    Tipos primitivos: Tipo boolean

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -2,34 +2,24 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-        char miCaracter = 'A';
-        System.out.println("miCaracter = " + miCaracter);
-
-        //Lista Unicode
-        char varChar = '\u0022';
-        System.out.println("varChar = " + varChar);
-
-        char varDecimal = 35;
-        System.out.println("varDecimal = " + varDecimal);
-
-        char varSimbolo = '$';
-        System.out.println("varSimbolo = " + varSimbolo);
-
+        boolean varBoolean = false;
+        System.out.println("varBoolean = " + varBoolean);
         
-        //Inferencia con var en tipo char
-        var varChar2 = '\u0022';
-        System.out.println("varChar2 = " + varChar2);
-
-        var varDecimal2 = (char)35;
-        System.out.println("varDecimal2 = " + varDecimal2);
-
-        var varSimbolo2 = '$';
-        System.out.println("varSimbolo2 = " + varSimbolo2);
+        if(varBoolean) {
+            System.out.println("La bandera esta alzada");
+        }
+        else {
+            System.out.println("La bandera esta abajo");
+        }
         
+        var edad = 17;
         
-        //Convertir char a entero: Arrojara el decimal del unicode como resultado: 36
-        int variableEnteraSimbolo = '$';
-        System.out.println("variableEnteraSimbolo = " + variableEnteraSimbolo);
+        if(edad >=18) {
+            System.out.println("Eres mayor de edad");
+        }
+        else {
+            System.out.println("Eres menor de edad");
+        }
 
     }
 }
