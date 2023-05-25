23676c36092481fafd27a9e7add7fe32e554f3a7
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-25 00:05:30 -0600

    Tipos primitivos: Conversion de tipos, De String a Int

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -1,25 +1,22 @@
 
+import java.util.Scanner;
+
+
 public class HolaMundo {
 
     public static void main(String args[]) {
-        boolean varBoolean = false;
-        System.out.println("varBoolean = " + varBoolean);
+      //Convertir tipo String a tipo int
+      var edad = Integer.parseInt("20");
+        System.out.println("edad = " + (edad + 1));
         
-        if(varBoolean) {
-            System.out.println("La bandera esta alzada");
-        }
-        else {
-            System.out.println("La bandera esta abajo");
-        }
+        var valorPi = Double.parseDouble("3.1416");
+        System.out.println("valorPi = " + valorPi);
         
-        var edad = 17;
-        
-        if(edad >=18) {
-            System.out.println("Eres mayor de edad");
-        }
-        else {
-            System.out.println("Eres menor de edad");
-        }
+        //Pedir un valor
+        var consola = new Scanner(System.in);
+        System.out.println("Cual es tu edad?");
+        edad = Integer.parseInt(consola.nextLine());
+        System.out.println("edad = " + edad);
 
     }
 }
