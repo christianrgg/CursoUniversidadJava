05c66c7a1259ee15049a5eeb79056caa46f6c4a4
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-25 00:40:48 -0600

    Tipos primitivos: Conversion de tipos; De Int a String y de String a Char

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -13,10 +13,22 @@ public class HolaMundo {
         System.out.println("valorPi = " + valorPi);
         
         //Pedir un valor
-        var consola = new Scanner(System.in);
+        /*var consola = new Scanner(System.in);
         System.out.println("Cual es tu edad?");
         edad = Integer.parseInt(consola.nextLine());
-        System.out.println("edad = " + edad);
+        System.out.println("edad = " + edad);*/
+        
+        var edadTexto = String.valueOf(10);
+        System.out.println("edadTexto = " + edadTexto);
+        
+        //Extraer un caracter de una cadena y asignarlo a una variable char
+        var caracter = "hola".charAt(0);
+        System.out.println("caracter = " + caracter);
+        var consola = new Scanner(System.in);
+        System.out.println("Proporciona un caracter");
+        caracter = consola.nextLine().charAt(0);
+        System.out.println("caracter = " + caracter);
+        
 
     }
 }
