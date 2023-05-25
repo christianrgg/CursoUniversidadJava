3f4f204fda0c10eda47ecda21f68556aa196fcd6
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-24 20:52:01 -0600

    Tipos primitivos: Inferencia de tipos con var

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -2,18 +2,15 @@
 public class HolaMundo {
 
     public static void main(String args[]) {
-       /*
-        Tipos primitivos flotantes: float y double
-        */
-       
-       float numeroFloat = 3.4028235E38F;
+       var numeroEntero = 10;
+        System.out.println("numeroEntero = " + numeroEntero);
+        
+        var numeroFloat = 10.12;
         System.out.println("numeroFloat = " + numeroFloat);
-        System.out.println("Valor minimo tipo float" + Float.MIN_VALUE);
-        System.out.println("Valor maximo tipo float" + Float.MAX_VALUE);
-       
-        double numeroDouble = 1.7976931348623157E308;  //Soporta enteros y decimales
-        System.out.println("numeroDouble = " + numeroDouble);
-        System.out.println("Valor minimo tipo double" + Double.MIN_VALUE);
-        System.out.println("Valor maximo tipo double" + Double.MAX_VALUE);
+        
+        var numeroFloat2 = 10.0F;
+        System.out.println("numeroFloat2 = " + numeroFloat2);
+        
+        
     }
 }
