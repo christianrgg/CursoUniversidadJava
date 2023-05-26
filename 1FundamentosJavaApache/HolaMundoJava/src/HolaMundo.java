c1a089e27c5beb563dc07b1a80396ed81400b01b
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-25 20:50:26 -0600

    Sentencias de control: if - else

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -4,16 +4,11 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        System.out.println("Ingresa el primer valor:");
-        Scanner consola = new Scanner(System.in);
-        int valor1 = Integer.parseInt(consola.nextLine());
-        System.out.println("Ingresa el segundo valor:");
-        var valor2 = Integer.parseInt(consola.nextLine());
-        if (valor1 > valor2)  {
-            System.out.println("En número mayor es " + valor1);
-        }
-        else {
-            System.out.println("El número mayor es " + valor2);
+        var condicion = true;
+        if(condicion) {
+            System.out.println("Condicion verdadera");
+        } else {
+            System.out.println("Condición falsa");
         }
     }
 }
