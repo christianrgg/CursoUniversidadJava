aab41d4854949e1a3227372767074c56158b8f71
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-29 12:10:24 -0600

    Ciclos en JAVA: Do while

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -2,12 +2,26 @@
 import java.util.Scanner;
 
 public class HolaMundo {
-
+    
     public static void main(String args[]) {
+//        Ciclo WHILE
+//        var contador = 0;
+//        while(contador < 3){ //Seleccionar contador < 3 y hacer clic derecho opción  "New Watch" y debuggear
+//            System.out.println("contador = " + contador);
+//            contador++;
+//        }
+
+
+        //Ciclo DO WHILE
         var contador = 0;
-        while(contador < 3){
+        do {
             System.out.println("contador = " + contador);
             contador++;
-        }
+        }while(contador <3);
     }
+    
+    //Usar While: Si queremos que la condición se compruebe desde un inicio y no se ejecute nada del código del ciclo en caso de que la condición sea falsa
+    //Usar Do While: En caso de que necesitemos que se ejecute al menos una vez este bloque de código del ciclo independientemente de que la condición sea verdadera o falsa. (La condición se verifica al final de cada iteración por eso al mensuna vez sera ejecutará)
+    
+    // En resumen, el bucle while se utiliza cuando se quiere repetir un bloque de código mientras se cumpla una condición, mientras que el bucle do-while se utiliza cuando se desea ejecutar un bloque de código al menos una vez y luego repetirlo mientras se cumpla una condición.
 }
