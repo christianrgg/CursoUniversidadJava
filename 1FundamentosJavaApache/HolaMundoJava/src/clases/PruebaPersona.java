75b73d8f1386d869eb4d87a9851d226a3224b2ad
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-29 15:09:20 -0600

    Clases en JAVA: Objetos, información adicional

--- 1FundamentosJavaApache/HolaMundoJava/src/clases/PruebaPersona.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/clases/PruebaPersona.java
@@ -2,15 +2,19 @@
 package clases;
 
 public class PruebaPersona {
-    public static void main(String[] args) {
-        //Se llama la clase Persona, Se define el nombre del objeto
-        Persona persona1;
-        
-        //persona1 llama al constructor Persona para crear un nuevo objeto (Recordar windows navigator)
-        persona1 = new Persona();
-        //Asignar los atributos del nuevo objeto persona1
+    public static void main(String[] args) {      
+        Persona persona1 = new Persona(); // Esta es la forma de hacerlo en una sola linea
         persona1.nombre = "Juan";
         persona1.apellido = "Gutierrez";
         persona1.desplegarInformación();
+        
+        //Para obtener ubicación en memoria
+        Persona persona2 = new Persona();
+        System.out.println("persona2 = " + persona2); //Consola: persona2 = clases.Persona@15aeb7ab   nombredelpaquete + nombre de la clase + ubicación en memoria
+        
+        persona2.desplegarInformación();
+        persona2.nombre = "Saira";
+        persona2.apellido = "Hernandez";
+        persona2.desplegarInformación();
     }
 }
