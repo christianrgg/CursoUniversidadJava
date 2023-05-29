b69b7f4515fa261daacabccb80f22e190aba5d45
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-29 14:05:19 -0600

    Clases en JAVA

---
+++ 1FundamentosJavaApache/HolaMundoJava/src/clases/Persona.java
@@ -0,0 +1,14 @@
+//Para crear una clase
+//    1. Clic derecho sobre "Source Packages"
+//    2. Opción "New" y Java Class
+//    3. Colocar el nombre de la clase
+//    4. En input packages escribir clases. 
+
+
+package clases;
+
+//La primer letra de cada palabra debe estar en mayuscula
+
+public class Persona {
+    
+}
