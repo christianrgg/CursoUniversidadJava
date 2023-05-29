4b98729a5a8ff9a583610619152301a8263bba95
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-29 11:51:01 -0600

    Ciclos en JAVA: While

--- 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
+++ 1FundamentosJavaApache/HolaMundoJava/src/HolaMundo.java
@@ -4,37 +4,10 @@ import java.util.Scanner;
 public class HolaMundo {
 
     public static void main(String args[]) {
-        //INSTRUCCION
-        System.out.println("Ingresa calificación del alumno");
-        //LLANADO CLASE CONSOLA
-        Scanner consola = new Scanner(System.in);
-        //CONVIRTIENDO DATOS DE LA CONSOLA
-        var calificación = Integer.parseInt(consola.nextLine());
-        
-        var letraCalificacion= "Caso no encontrado";
-
-        switch(calificación) {
-            case 10:
-                letraCalificacion = "A";
-                break;
-            case 9:
-                letraCalificacion = "B";
-                break;
-            case 8: 
-                letraCalificacion = "C";
-                break;
-            case 7:
-                letraCalificacion = "D";
-                break;
-            case 6:
-                letraCalificacion = "E";
-                break;
-            case 5: case 4: case 3: case 2: case 1: case 0:
-                letraCalificacion = "F";
-                break;
-            default:
-                letraCalificacion = "No aplica";
+        var contador = 0;
+        while(contador < 3){
+            System.out.println("contador = " + contador);
+            contador++;
         }
-        System.out.println("letraCalificacion = " + letraCalificacion);
     }
 }
