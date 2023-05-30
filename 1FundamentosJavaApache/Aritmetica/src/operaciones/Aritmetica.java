dde9ef37f1098ef3806d0ac7cb09a6c78e8c4d30
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-30 08:58:31 -0600

    Metodos en JAVA: Estructura

---
+++ 1FundamentosJavaApache/Aritmetica/src/operaciones/Aritmetica.java
@@ -0,0 +1,14 @@
+package operaciones;
+
+public class Aritmetica {
+    //Atributos de la clase
+    int a;
+    int b;
+    
+    //Metodo
+    //primera letra minutscula, de ahi uppercase
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+}
