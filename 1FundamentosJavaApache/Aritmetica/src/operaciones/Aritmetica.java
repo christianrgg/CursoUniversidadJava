dba089e90b255580ee1ee28a0ad450bffda9dcfe
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-31 11:11:48 -0600

    Metodos en JAVA: Uso del operador this

--- 1FundamentosJavaApache/Aritmetica/src/operaciones/Aritmetica.java
+++ 1FundamentosJavaApache/Aritmetica/src/operaciones/Aritmetica.java
@@ -21,18 +21,12 @@ public class Aritmetica {
     }
     
     //Metodo con argumentos
-    public int sumarConArgumentos(int arg1, int arg2){
-        a = arg1;
-        b= arg2;
+    public int sumarConArgumentos(int a, int b){
+        this.a = a;
+        this.b= a;
 //        return a+b;
-        return sumarConRetorno();
+        return this.sumarConRetorno();
     }
 }
 
-//PASOS PARA DEBUGUEAR
-//1. Pon el punto de ruptura
-//2. Clic derecho "debug file"
-//3. Ir visualizando la ejecución
-//4. Para entrar a algun metodo hacer doble clic sobre y debe seleccionarse
-//5. Luego clic en boton "Step Into F7"
-//6. Seguir debuggueando con "Step Over F8"
+// this hace referencia a un atributo de nuestra clase y no de nuestra variable local
