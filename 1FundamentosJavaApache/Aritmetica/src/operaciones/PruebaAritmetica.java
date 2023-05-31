f635e07cee461bb60b195e57792e7ea8f70bb5cb
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-05-31 13:50:43 -0600

    Alcance de variables: Memoria stack y heap

--- 1FundamentosJavaApache/Aritmetica/src/operaciones/PruebaAritmetica.java
+++ 1FundamentosJavaApache/Aritmetica/src/operaciones/PruebaAritmetica.java
@@ -7,11 +7,11 @@ public class PruebaAritmetica {
     public static void main(String[] args) {
         
         //Variables locales solo pueden usar var
-        var a = 10;
-        var b = 2;
+        var a = 10; //Se almacenan en la memoria stack
+        var b = 2; //Se almacenan en la memoria stack
         miMetodo();
         
-        Aritmetica aritmetica1 = new Aritmetica();
+        Aritmetica aritmetica1 = new Aritmetica(); //Se almacena en la memoria heap
         System.out.println("aritmetica1 a:" + aritmetica1.a);
         System.out.println("aritmetica1 b:" + aritmetica1.b);
         
@@ -28,6 +28,7 @@ public class PruebaAritmetica {
         
     }
     
-    
+    //Variables locales se almacenan en la memoria stack
+    //Vatiables tipo objeto se almacenan en la memoria heap
     
 }
